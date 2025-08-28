8a0b92c551f6a53799ceab0c1976aa2c7b33a2d4
sidietz <simon.dietz@plantwatch.de>
2025-08-28 21:41:49 +0200

    add index.html

---
+++ src/main/java/de/oberamsystems/slm/controller/IndexController.java
@@ -0,0 +1,13 @@
+package de.oberamsystems.slm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
