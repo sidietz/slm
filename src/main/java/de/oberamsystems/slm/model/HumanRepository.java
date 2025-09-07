6d739a10baf253b8b31ae3373495423398f465e6
sidietz <simon.dietz@plantwatch.de>
2025-09-07 23:28:38 +0200

    remove newline

--- src/main/java/de/oberamsystems/slm/model/HumanRepository.java
+++ src/main/java/de/oberamsystems/slm/model/HumanRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface HumanRepository extends JpaRepository<Human, Long> {
 
 	public Human findFirstByOrderByDaysUntilBirthdayAsc();
-
 	public List<Human> findAllByOrderByDaysUntilBirthdayAsc();
 }
