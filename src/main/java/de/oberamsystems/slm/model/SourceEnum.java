7b133ddd5bad73d6e87bb06bb5fa51a1b613dae8
sidietz <simon.dietz@plantwatch.de>
2025-09-08 17:00:46 +0200

    add learning

---
+++ src/main/java/de/oberamsystems/slm/model/SourceEnum.java
@@ -0,0 +1,5 @@
+package de.oberamsystems.slm.model;
+
+public enum SourceEnum {
+	BOOK, COURSE, LECTURE, TUTORIAL, YOUTUBE
+}
---
+++ src/main/java/de/oberamsystems/slm/model/StatusEnum.java
@@ -0,0 +1,5 @@
+package de.oberamsystems.slm.model;
+
+public enum StatusEnum {
+	PLANNED, IN_PROGRESS, COMPLETED
+}
