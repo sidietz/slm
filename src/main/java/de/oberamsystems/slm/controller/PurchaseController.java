fe92260b7af2eb650deb5336fa5121f658ddddfe
sidietz <simon.dietz@plantwatch.de>
2025-10-18 22:44:46 +0200

    minor fix

--- src/main/java/de/oberamsystems/slm/controller/PurchaseController.java
+++ src/main/java/de/oberamsystems/slm/controller/PurchaseController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class PurchaseController {
 	@GetMapping("/purchase")
 	public String getpurchase(Model model) {
 		
-		List<Purchase> purchases = repo.findAll();
+		List<Purchase> purchases = repo.findAll(Sort.by("purchaseDate"));
 		float totalPrice = 0;
 		
 		for (Purchase p : purchases) {
